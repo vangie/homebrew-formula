556851246968e4bdd603e068c6f83cd6105c0d47
vangie <duwan@live.com>
2018-02-13 17:54:25 +0800

    add checksum for shell completion file.

--- fcli.rb
+++ fcli.rb
@@ -10,9 +10,11 @@
 
   resource "bash_completion" do
     url "https://raw.githubusercontent.com/aliyun/fcli/master/misc/completion/fcli-completion.bash"
+    sha256 "f8ef4d2843bb5ca261f3c0735d2b40e52fccba639f92f2601a92e3cf6224195a"
   end
   resource "zsh_completion" do
     url "https://raw.githubusercontent.com/aliyun/fcli/master/misc/completion/_fcli"
+    sha256 "2e28f81c0f15fddab6f9ef8976bfd6dff2325f51eca6b9e9dc5fd8f48278c651"
   end
 
   def install
