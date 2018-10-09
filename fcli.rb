de35e7079963d6b62ddb605538d0666e54d2196d
kevin <subo.ysb@alibaba-inc.com>
2018-10-09 21:16:29 +0800

    fcli 0.22

--- fcli.rb
+++ fcli.rb
@@ -1,27 +1,26 @@
 class Fcli < Formula
-  desc "The command line interface of function compute of aliyun."
+  desc "Official Function Compute of Aliyun command-line interface"
   homepage "https://github.com/aliyun/fcli"
-  version "0.20"
-  url "https://gosspublic.alicdn.com/fcli/fcli-v#{version}-darwin-amd64.zip" 
-  sha256 "07c6fb5f1697352981707347bebb6902f95291b957b4b15e6d3203a8ceaff2eb"
+  version "0.22"
+  url "https://gosspublic.alicdn.com/fcli/fcli-v#{version}-darwin-amd64.zip"
+  sha256 "2779f54ea1d2a0beeab8da78ade89154b570fcfc4dc57259b98689a0790b6bd1"
 
   resource "bash_completion" do
-    url "https://raw.githubusercontent.com/aliyun/fcli/master/misc/completion/fcli-completion.bash"
-    sha256 "f8ef4d2843bb5ca261f3c0735d2b40e52fccba639f92f2601a92e3cf6224195a"
+    url "https://raw.githubusercontent.com/aliyun/fcli/a8de528af6ecc20e5cc48b04dd6fca5b48ecee82/misc/completion/fcli-completion.bash"
+    sha256 "87975b3d120fff92bf2cff668c90641e9fd67d8d2e33aa54c58c27c6287bcfb9"
   end
   resource "zsh_completion" do
-    url "https://raw.githubusercontent.com/aliyun/fcli/master/misc/completion/_fcli"
-    sha256 "2e28f81c0f15fddab6f9ef8976bfd6dff2325f51eca6b9e9dc5fd8f48278c651"
+    url "https://raw.githubusercontent.com/aliyun/fcli/a8de528af6ecc20e5cc48b04dd6fca5b48ecee82/misc/completion/_fcli"
+    sha256 "01ff7be3af72245bc3d0ae581aa2e29361b9ee7e625314ed54442b79c5665414"
   end
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     bin.install "fcli"
     resource("bash_completion").stage { bash_completion.install "fcli-completion.bash" }
     resource("zsh_completion").stage { zsh_completion.install "_fcli" }
   end
 
   test do
-    system "false"
+    assert_match "fcli", shell_output("#{bin}/fcli help")
   end
 end
