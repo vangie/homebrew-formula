253ed178c127587a484ec93629bd702c2799961b
vangie <duwan@live.com>
2019-03-05 17:28:26 +0800

    upgrade fun to 2.10.2

--- fun.rb
+++ fun.rb
@@ -1,9 +1,9 @@
  class Fun < Formula
   desc "Fun is a development tool for serverless applications of aliyun."
   homepage "https://github.com/aliyun/fun"
-  version "2.10.1"
+  version "2.10.2"
   url "https://gosspublic.alicdn.com/fun/fun-v#{version}-macos-x64.zip"
-  sha256 "b86ac910020f1b9d5ee378dfd7c1fab24ccf1f9857cf60a49f76f870c97801be"
+  sha256 "6325d9a2cfcf75607d1ba2b03403c4077d0eb48988c7af1665ad3398b9a99716"
 
 
   def install
