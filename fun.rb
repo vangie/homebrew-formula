46370db6a901ed875b92bc6ff19b93f59a164e5a
vangie <duwan@live.com>
2019-10-12 09:51:56 +0800

    upgrade fun to 3.0.4

--- fun.rb
+++ fun.rb
@@ -1,13 +1,13 @@
  class Fun < Formula
   desc "Fun is a development tool for serverless applications of aliyun."
   homepage "https://github.com/alibaba/funcraft"
-  version "3.0.3"
-  url "https://gosspublic.alicdn.com/fun/fun-v#{version}-macos-x64.zip"
-  sha256 "db9677e1550fa9a5f1fd0fbaf352154ab930b441c054f0ac1319dd67934f2d31"
+  version "3.0.4"
+  url "https://gosspublic.alicdn.com/fun/fun-v#{version}-macos.zip"
+  sha256 "ee9a75cbfe754e4cdf48a53f894d1fdac0a5d8dd7c3a42f52a37be137f96a256"
 
 
   def install
-    bin.install "fun-v#{version}-macos-x64" => "fun"
+    bin.install "fun-v#{version}-macos" => "fun"
   end
 
   test do
