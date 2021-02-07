fc166a04a4595d834015d1cd55477aa2799938d2
rsonghuster <ls_huster@163.com>
2021-02-07 10:44:33 +0800

    update new version

--- fun.rb
+++ fun.rb
@@ -1,7 +1,7 @@
  class Fun < Formula
   desc "Fun is a development tool for serverless applications of aliyun."
   homepage "https://github.com/alibaba/funcraft"
-  version "3.6.9"
+  version "3.6.21"
   url "https://gosspublic.alicdn.com/fun/fun-v#{version}-macos.zip"
   sha256 "6a216d8064d63956dc0ba1ca8f3ee60d99225b1c6ae01e3c404011c451593d7f"
 
--- ossutil.rb
+++ ossutil.rb
@@ -1,8 +1,8 @@
 class Ossutil < Formula
     desc "The command line interface of oss of aliyun."
     homepage "https://github.com/aliyun/ossutil"
-    version "1.4.1"
-    url "http://docs-aliyun.cn-hangzhou.oss.aliyun-inc.com/assets/attach/50452/cn_zh/1524644116085/ossutilmac64" 
+    version "1.7.1"
+    url "http://gosspublic.alicdn.com/ossutil/1.7.1/ossutilmac64"
     sha256 "f8a3f7cb8c2c3f7125dfa96cab5c88e2548c334cf1573938a13e8cbd73f28a96"
 
 
