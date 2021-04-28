3783d611a99a21ac3489ab3e69b9470b1a526d76
千风 <qianfeng.zqf@alibaba-inc.com>
2021-04-28 15:03:59 +0800

    change fun source url; update fun version to 3.6.22

--- fun.rb
+++ fun.rb
@@ -1,10 +1,9 @@
  class Fun < Formula
   desc "Fun is a development tool for serverless applications of aliyun."
   homepage "https://github.com/alibaba/funcraft"
-  version "3.6.21"
-  url "https://gosspublic.alicdn.com/fun/fun-v#{version}-macos.zip"
-  sha256 "d88ba24d9f1f812fc7c69ec90cffc51a411f3a4368da92bf3101505cca97d90b"
-
+  version "3.6.22"
+  url "http://funcruft-release.oss-accelerate.aliyuncs.com/fun/fun-v#{version}-macos.zip"
+  sha256 "fea656d21e8a71c8b24a40d75977405d371a50ad666475f341f5226309085600"
 
   def install
     bin.install "fun-v#{version}-macos" => "fun"
