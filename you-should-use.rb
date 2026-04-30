bb0247b2943ec82f3cc30c18814e6edb56ac1e9e
Vangie Du <duwan@live.com>
2026-04-30 08:17:12 +0000

    you-should-use v0.3.0

--- you-should-use.rb
+++ you-should-use.rb
@@ -1,7 +1,7 @@
 class YouShouldUse < Formula
   desc "Shell plugin that suggests aliases and modern command alternatives"
   homepage "https://github.com/vangie/you-should-use"
-  url "https://github.com/vangie/you-should-use.git", tag: "v0.2.0"
+  url "https://github.com/vangie/you-should-use.git", tag: "v0.3.0"
   license "MIT"
   head "https://github.com/vangie/you-should-use.git", branch: "main"
 
